6d5f9aaf388c5141af749184347b25d8d7b23e4f
Hitanshi202 <147725309+Hitanshi202@users.noreply.github.com>
2024-06-27 13:51:01 +0530

    Add files via upload

---
+++ core java/ReversePrint.java
@@ -0,0 +1,18 @@
+package codes;
+
+import java.util.Scanner;
+
+public class ReversePrint {
+
+	public static void main(String[] args) 
+	{
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number");
+    int n = sc.nextInt();
+    for(int i = 10;i>=1;i--)
+    {
+    	System.out.println(i);
+    }
+	}
+
+}
